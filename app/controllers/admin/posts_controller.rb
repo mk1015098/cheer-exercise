1e97c6764fd442249b0ca2a134a2a37c00d3344a
MoekaKakimoto <k.moeka0984@gmail.com>
2023-01-13 11:54:08 +0000

    [Add]admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,14 @@
 class Admin::PostsController < ApplicationController
+
   def index
+    @posts = Post.all
+    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
+    @post_tags = @post.tags
+    @user = @post.user
+    @post_comment = PostComment.new
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,11 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @post = Post.new
   end
 end
