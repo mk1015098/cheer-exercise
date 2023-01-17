a6e75471d3081ae89f908eea94b00ac77a60b167
MoekaKakimoto <k.moeka0984@gmail.com>
2023-01-17 12:06:08 +0000

    [Add]bootstrap

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -33,7 +33,7 @@ class User::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :image)
+    params.require(:user).permit(:name, :image, :introduction)
   end
 
   def ensure_guest_user
