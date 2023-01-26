8e077af4f5fef241a9793177be1e3d600dab6d3e
MoekaKakimoto <k.moeka0984@gmail.com>
2023-01-26 11:38:51 +0000

    [Update]integer

--- db/migrate/20230107061526_create_post_tags.rb
+++ db/migrate/20230107061526_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[6.1]
   def change
-    create_table :post_tags do |t|
-      t.references :post, type: :integer, foreign_key: true
-      t.references :tag, type: :bigint, foreign_key: true
+    create_table :post_tags, id: :integer do |t|
+      t.integer :post, foreign_key: true
+      t.integer :tag, foreign_key: true
       t.timestamps
     end
     # 同じタグを２回保存できないようにする
