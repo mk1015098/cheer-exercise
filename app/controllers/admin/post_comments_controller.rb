48cead605d784ea6c8a651c6e8eff5f541fe08da
MoekaKakimoto <k.moeka0984@gmail.com>
2023-01-31 05:14:25 +0000

    [Fix]admin/comment

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,10 +1,16 @@
 class Admin::PostCommentsController < ApplicationController
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @post_comment = PostComment.find_by(id: params[:id], post_id: params[:post_id])
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.find_by(params[:id], post_id: params[:post_id])
     @post_comment.destroy
     redirect_to admin_post_path(@post.id)
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
   end
 
 end
